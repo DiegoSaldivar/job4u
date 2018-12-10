11665284542ca47e25e2229158084f747f9d29c4
codarexer <43453827+codarexer@users.noreply.github.com>
2018-12-10 16:22:13 +0100

    form changed to Symfony

--- src/Form/UserFormType.php
+++ src/Form/UserFormType.php
@@ -10,25 +10,24 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class UserFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            
-            ->add(
-                'password', 
-                RepeatedType::classs,
-                array(
-                    'type' => PasswordType::class,
-                    'invalid_message' => 'The password filds must match.',
+            ->add('fullName', TextType::class)
+            ->add('eMail', EmailType::class)
+            ->add('username', TextType::class)
+            ->add('password', RepeatedType::class, array
+                   ('type' => PasswordType::class,'invalid_message' => 'The password fields must match.',
                     'options' => array('attr' => array('class' => 'password-field')),
                     'required' => true,
-                    'first_optios' => array('label' => 'Password'),
+                    'first_options' => array('label' => 'Password'),
                     'second_options' => array('label' => 'Repeat Password')
                  )
-               )->add('username', TextType::class);
+               );
                 
                if ($options['standalone'])
                {
