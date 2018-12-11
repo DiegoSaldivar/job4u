3755866401d0c354e4725e73827ac9bc500f7aaf
Diego Saldivar <dssaldivar@hotmail.com>
2018-12-11 16:49:03 +0100

    Add check fields in main.css

--- public/js/validateFieldsForm.js
+++ public/js/validateFieldsForm.js
@@ -1,47 +1,83 @@
-function validateForm() { // SECURITY
-
+$(function () { 
+	
+	// 	 fields of the Register Form
+	//	 #user_form_fullname
+	//	 #user_form_email
+	//	 #user_form_username
+	//	 #user_form_password_first
+	//	 #user_form_password_second
+	
     // Apply a display block to #pswd_info on focus on password
-    $('#pswd').on('focus', function () {
+    $('#user_form_password_first').on('focus', function () {
         $('#pswd_info').css('display', 'block');
     });
 
 
     //display none to #pswd_info when blur
-    $('#pswd').on('blur', function () {
+    $('#user_form_password_first').on('blur', function () {
         $('#pswd_info').css('display', 'none');
     });
-
-
-    // on every key pressed
+	
+    
+	// on every key pressed
     // checked the password value
-    $('#pswd').on('keyup', function () {
-        //Gathering
-        const passValue = $(this).val(); // this refere to the element that trigger the event
-        console.log(passValue);
-        //Logic
-        // length >= 8
-        const lengthCheck = (passValue.length >= 8);
-        // at least one letter str.match(/[A-z]/)
-        const oneLetterCheck = passValue.match(/[A-z]/);
-        // at least one Capital letter str.match(/[A-Z]/)
-        const oneCapitalCheck = passValue.match(/[A-Z]/);
-        // at least one number str.match(/\d/)
-        const numberCheck = passValue.match(/\d/);
-
-        //display
-        check('#length', lengthCheck);
-        check('#letter', oneLetterCheck);
-        check('#capital', oneCapitalCheck);
-        check('#number', numberCheck);
-
-
-    });
-
-    $('form').on('submit', function (event) {
+	 $('#user_form_fullname').on('keyup', function () {
+		 
+		 const passValue = $(this).val(); // this refere to the element that trigger the event
+	     console.log(passValue);
+	     //Logic
+	     // length >= 8
+	     const lengthCheck = (passValue.length >= 8);
+	     // at least one letter str.match(/[A-z]/)
+	     const oneLetterCheck = passValue.match(/[A-z]/);
+	     // at least one Capital letter str.match(/[A-Z]/)
+	     const oneCapitalCheck = passValue.match(/[A-Z]/);
+	    
+	
+	     //display
+	     check('#length', lengthCheck);
+	     check('#letter', oneLetterCheck);
+	     check('#capital', oneCapitalCheck);
+	  		 
+	 })
+ 
+ 
+	 $('#user_form_email').on('keyup', function () {
+		 const passValue = $(this).val(); // this refere to the element that trigger the event
+	     console.log(passValue);
+	     const emailFormat = passValue.match(/\S+@+\S+\.\S+/); // Pattern that must have the input email 
+	     
+	     check('#Format', emailFormat);
+		 
+	 })
+ 
+	 
+	 
+	// on every key pressed
+    // checked the password value
+	 $('#user_form_username').on('keyup', function () {
+		 
+		 const passValue = $(this).val(); // this refere to the element that trigger the event
+	     console.log(passValue);
+	     //Logic
+	     // length >= 8
+	     const lengthCheck = (passValue.length >= 8);
+	     // at least one letter str.match(/[A-z]/)
+	     const oneLetterCheck = passValue.match(/[A-z]/);
+	     	   	
+	     //display
+	     check('#length', lengthCheck);
+	     check('#letter', oneLetterCheck);
+	     
+	    		 
+	 })
+ 
+ 
+ 	$('user_form').on('submit', function (event) {
         event.preventDefault();
         //Gather
-        const passValue = $('#pswd').val();
-        const passConfirm = $('#pswdConfirm').val();
+        const passValue = $('#user_form_password_first').val();
+        const passConfirm = $('#user_form_password_second').val();
         const sameCheck = (passConfirm === passValue)
         //console.log(passValue + '/' + passConfirm + '=>' + sameCheck);
         const lengthCheck = (passValue.length >= 8);
@@ -58,20 +94,22 @@ function validateForm() { // SECURITY
         }
 
     })
+    
     // on submit 
     //if confirmation password is same as password 
     //and password check all the cases
     //Replace the form with "Success"
-
+ 
     function check(selector, validCheck) {
-        //if ok => class change to "valid"
-        // else class change to "invalid"
-        if (validCheck) {
-            $(selector).addClass('valid');
-            $(selector).removeClass('invalid');
-        } else {
-            $(selector).addClass('invalid');
-            $(selector).removeClass('valid');
-        }
-    }
-} // END OF JQUERY
+	     //if ok => class change to "valid"
+	     // else class change to "invalid"
+	     if (validCheck) {
+	         $(selector).addClass('valid');
+	         $(selector).removeClass('invalid');
+	     } else {
+	         $(selector).addClass('invalid');
+	         $(selector).removeClass('valid');
+	     }
+	}
+
+}); // END OF JQUERY
