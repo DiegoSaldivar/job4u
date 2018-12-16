1fb8a8ff36476458f617cbe8052f7a99b97555c4
Max Nussbaum <max.nussbaum012@gmail.com>
2018-12-16 14:56:41 +0100

    Security features implemented. Users don't have access to the login anymore when logged in.

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -90,7 +90,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
         // For example : return new RedirectResponse($this->router->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->router->generate('userdash'));
+        return new RedirectResponse($this->router->generate('app_login'));
     }
 
     protected function getLoginUrl()
